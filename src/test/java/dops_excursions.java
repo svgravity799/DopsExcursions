09671307e2d3ac9f5f2e412ff1e7b76468be697a
pavelyatmanov <svgravity799@gmail.com>
2025-03-26 18:16:23 -0700

    Excursions - v1notbron

--- src/test/java/dops_excursions.java
+++ src/test/java/dops_excursions.java
@@ -1,11 +1,16 @@
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.WebDriverRunner;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
+import java.util.Set;
+import java.time.Duration;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static java.awt.Color.yellow;
 
 
 public class dops_excursions {
@@ -18,42 +23,108 @@ public class dops_excursions {
       Configuration.holdBrowserOpen = true;
       Configuration.timeout = 20000; // Устанавливаем таймаут в 20 секунд
 
-      open("https://www.fstravel.com/searchexcursions");
+      open("http://fstravel.com/searchexcursions");
 
-      //  if ($("[class=popmechanic-reset]").isDisplayed()) { // закрытие рекламного баннера
-      //  $("[class=кpopmechanic-close]").click();
-  //}
-
+       //Авторизация
         $("[class=v-icon-user-14]").click();
-        $("[class=form__field]").setValue("testfspavelclient@yandex.ru");
+        $("[class=form__field]").setValue("testfspavelclient09@yandex.ru");
         $("[id=password]").setValue("Pavelpa42!!!");
         $("[type=submit]").click();
+
+        //Поиск
 $(byText("Страна поездки")).click();
 $(byText("Россия")).click();
 $(byText("Выбрать")).click();
-      $("[class=calendar-popup__arrow-right").click();
-      $("[class=calendar-popup__arrow-right").click();
-      $("[class=calendar-popup__arrow-right").click();
-      $("[class=calendar-popup__arrow-right").click();
+      $("[class=calendar-popup__arrow-right]").click();
+      $("[class=calendar-popup__arrow-right]").click();
+      $("[class=calendar-popup__arrow-right]").click();
+      $("[class=calendar-popup__arrow-right]").click();
       $(byText("12")).click();
       $(byText("13")).click();
+     $(byText("Найти")).click();
       $(byText("Найти")).click();
+
+    //Попап
       $("[class=excursion-card__content-btn]").click();
 $(byText("Выбрать")).click();
       $(byText("12")).click();
+      sleep(1500);
       $("[class=excursion-modal-main__buttons-btn").click();
       $("[class=excursion-modal-main__buttons-btn").click();
-//$(byText("Адрес проживания на курорте")).setValue("Автотесты 17");
-//$("[id=password]").setValue("Pavelpa42!!!");
-      $("[class=v-icon-user-14]").click();
 
-      //   $(byText("searchexcursions")).click();
 
-    //   $("[class=v-chevron-right").click();
-     //  $("[class=popularCategories__item-icon").click();
 
-      //  $("[class=popularCategories__item-icon").click();
-//$(byText("https://api.selcdn.ru/v1/SEL_50469/funandsun-cdn/84f7da10-266c-4afa-9747-d3065fc313d2.svg")).click();  //выбордаты
+      //  Ждем, пока не откроется новая вкладка (макс 5 секунд)
+      Selenide.Wait()
+              .withTimeout(Duration.ofSeconds(5))
+              .until(d -> d.getWindowHandles().size() > 1);
+
+//  Получаем все дескрипторы вкладок
+      Set<String> tabs = WebDriverRunner.getWebDriver().getWindowHandles();
+      String originalTab = WebDriverRunner.getWebDriver().getWindowHandle();
+
+//  Находим новую вкладку (не равную оригинальной)
+      String newTab = tabs.stream()
+              .filter(tab -> !tab.equals(originalTab))
+              .findFirst()
+              .orElseThrow(() -> new RuntimeException("Новая вкладка не найдена"));
+
+//  Переключаемся на новую вкладку
+      switchTo().window(newTab);
+
+      //sleep(10000);
+
+
+    //  $("[class=popmechanic-close]").click();
+      $("[class=form__field]").click();
+      $("[class=form__field]").setValue("Похоже получилось");
+
+     // sleep(1000);
+
+      $("[name=lastName]").setValue("Автотестимя");
+      $("[name=firstName]").setValue("Автотестимя");
+      $("[name=middleName]").setValue("Автотестимя");
+
+      Selenide.actions()
+              .sendKeys(Keys.TAB)  // Первый TAB
+              .sendKeys(Keys.TAB)  // Второй TAB
+              .perform();
+
+      // Проверяем куда перешел фокус:
+      System.out.println($(":focus").getAttribute("class")); // Выведет класс активного элемента
+
+// Вводим данные:
+      $(":focus").setValue("3522");
+
+      Selenide.actions()
+              .sendKeys(Keys.TAB)
+              .perform();
+
+      System.out.println($(":focus").getAttribute("class"));  // - спросить как пишется строка
+      $(":focus").setValue("581929");
+
+
+
+
+     // $(byText("Cерия документа")).click();
+    //  $(byText("Cерия документа")).click();
+    //  $(byText("Cерия документа")).setValue("425");
+
+
+
+
+
+    //  $("[class=traveller-field]").setValue("174234");
+   //   sleep(1000);
+
+
+
+
+  //    $("[class=form__field]").setValue("Похоже получилось1");
+    //  $(byText("Серия документа")).click();
+  //$(byText("Cерия документа")).setValue("19");
+
+
 
 
 
